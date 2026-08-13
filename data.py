c41f2be7a4a5f8e4458a084e474e8c341069ad49
kennan neo <kennanneo@gmail.com>
2026-08-13 18:18:51 +0800

    able to get all 3k videos from requests but somewhere along the line it stops and only gives 13 in the end, need to work out way to only run the request once per test

--- data.py
+++ data.py
@@ -1,12 +1,9 @@
 #this will be where i get adn store the data from youtube
 from dotenv import load_dotenv
 import os
-import requests
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import pandas as pd
-import json
-import time
 from pprint import pprint
 
 load_dotenv()
@@ -27,62 +24,81 @@ lugwig_yt =youtube.channels().list(part = 'statistics,contentDetails,snippet',
                                   ).execute()
 #keys that i may want to index into 
 #relatedPlaylists, uploads
+#items is a list with one item so indexing 0 will give you everything
 upload_playlist_id = lugwig_yt['items'][0]['contentDetails']['relatedPlaylists']['uploads']
 
-playlist_items_request = youtube.playlistItems().list(
-    part = 'snippet,contentDetails',
-    playlistId = upload_playlist_id,
-    maxResults = 50 #edit to 50 after the rest of the stuff works
-) #http request object , when printed is giving the memory location in computer, need to execute to get the response
+next_page_token = None
+all_video_ids = []
 
-playlist_items_response = playlist_items_request.execute() #this one actually returns the videos 
+#use while to parse through all uploads
+while True:
+    playlist_items_request = youtube.playlistItems().list(
+        part = 'snippet,contentDetails',
+        playlistId = upload_playlist_id,
+        maxResults = 50, #edit to 50 after the rest of the stuff works
+        pageToken = next_page_token
+    ) #http request object , when printed is giving the memory location in computer, need to execute to get the response
 
-#index into the video stats from this video is under id
-#storing video id and position into dictionary
-video_ids = {}
-#api docs for playlist structure https://developers.google.com/youtube/v3/docs/playlistItems#resource
-for item in playlist_items_response['items']:
-    position = item['snippet']['position']
-    id = item['contentDetails']['videoId']
-    video_ids[position] = id
+    playlist_items_response = playlist_items_request.execute() #this one actually returns the videos 
 
-#make a video id list so that you only need to make the request once instead of 50 times, and then iterating over the list 
-video_ids_list = ','.join(video_ids.values()) #joins the keys not the values
+    #index into the video stats from this video is under id
+    #storing video id and position into dictionary
+    video_ids = {}
+    #api docs for playlist structure https://developers.google.com/youtube/v3/docs/playlistItems#resource
+    #append the 50 into the list
+    for item in playlist_items_response['items']:
+        all_video_ids.append(item['contentDetails']['videoId'])
+
+    #get next page token, get to get a dictionary value, if no next page token None is return
+    next_page_token = playlist_items_response.get('nextPageToken')
+
+    #break if no more page token not None = True
+    if not next_page_token:
+        break
+
+# need to batch the video id list to 50 each because the api will only take in 50 per call
 #need to get the video using video function, construct the object first
 #dict will give you the key, so id in this case will give the number and you need to index into the dictionary
-video_stats = youtube.videos().list(
-    part = 'snippet,statistics,contentDetails,LiveStreamingDetails',
-    id = video_ids_list
-)
-video_stats_response = video_stats.execute() #contains the batch of 50 videos
+for i in range(0, len(all_video_ids), 50):
+    chunk = all_video_ids[i:i+50]
+    id_string = ','.join(chunk) #convert to string so can pass as a parameter
 
-rows_data = [] #initialize list to store dict and then subsequently pass that as a pandas dataframe
-for video in video_stats_response['items']:
-    #items is a list with only one entry
-    title = video['snippet']['localized']['title']
-    views = video['statistics']['viewCount']
-    likes = video['statistics']['likeCount']
-    comment_count = video['statistics']['commentCount']
-    duration = video['contentDetails']['duration']
-    upload_time = video['snippet']['publishedAt']
-    is_vod = 'liveStreamingDetails' in video
+    video_stats = youtube.videos().list(
+        part = 'snippet,statistics,contentDetails,LiveStreamingDetails',
+        id = id_string
+    )
 
-    #append to dictionary
-    rows_data.append({
-        'title': title,
-        'views': views,
-        'likes': likes,
-        'comment_count': comment_count,
-        'duration': duration,
-        'upload_time': upload_time,
-        'is_vod': is_vod
-    })
+    video_stats_response = video_stats.execute() #contains the batch of 50 videos
+
+    rows_data = [] #initialize list to store dict and then subsequently pass that as a pandas dataframe
+    for video in video_stats_response['items']:
+        #items is a list with only one entry
+        title = video['snippet']['localized']['title']
+        views = video['statistics']['viewCount']
+        likes = video['statistics']['likeCount']
+        comment_count = video['statistics']['commentCount']
+        duration = video['contentDetails']['duration']
+        upload_time = video['snippet']['publishedAt']
+        is_vod = 'liveStreamingDetails' in video
+
+        #append to dictionary
+        rows_data.append({
+            'title': title,
+            'views': views,
+            'likes': likes,
+            'comment_count': comment_count,
+            'duration': duration,
+            'upload_time': upload_time,
+            'is_vod': is_vod
+        })
 
 #convert to pd dataframe
 df = pd.DataFrame(rows_data)
 
-print(df.head())
-    #pprint(video_stats_response)
+print(df)
+
+#consider converting to csv and just making the thing a function so i just need to run it once and its done
+#it almost works but is only calling out 13 videos now
 
 #filter out only videos from the list 'kind': 'youtube#video', currently includes shorts, 
 #maybe try using contentDetails.duration to filter out <5 mins and > 1hr videos
